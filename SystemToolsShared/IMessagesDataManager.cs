17826dcd0e513dc7277336b58ea1870f9a91c805
merabza <merabza@gmail.com>
2024-03-07 16:04:21 +0400

    202403071604

--- SystemToolsShared/IMessagesDataManager.cs
+++ SystemToolsShared/IMessagesDataManager.cs
@@ -5,6 +5,7 @@ namespace SystemToolsShared;
 
 public interface IMessagesDataManager
 {
+    void Dispose();
     Task SendMessage(string? userName, string message, CancellationToken cancellationToken);
     void UserConnected(string connectionId, string userName);
     void UserDisconnected(string connectionId, string userName);

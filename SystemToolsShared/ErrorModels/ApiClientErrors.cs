50e5141492a36ce130fb44726ace144ae3e475cb
merabza <merabza@gmail.com>
2024-03-13 06:36:21 +0400

    202403130635

--- SystemToolsShared/ErrorModels/ApiClientErrors.cs
+++ SystemToolsShared/ErrorModels/ApiClientErrors.cs
@@ -2,14 +2,17 @@
 
 public static class ApiClientErrors
 {
-    public static Err ApiReturnedAnError(string errorMessage) => new()
-        { ErrorCode = nameof(ApiReturnedAnError), ErrorMessage = $"Api Returned an Error: {errorMessage}" };
-
     public static readonly Err ApiUnknownError = new()
         { ErrorCode = nameof(ApiUnknownError), ErrorMessage = "Api returned an unknown error" };
 
     public static readonly Err ApiDidNotReturnAnything = new()
         { ErrorCode = nameof(ApiDidNotReturnAnything), ErrorMessage = "api did not return anything" };
+
+    public static Err ApiReturnedAnError(string errorMessage)
+    {
+        return new Err
+            { ErrorCode = nameof(ApiReturnedAnError), ErrorMessage = $"Api Returned an Error: {errorMessage}" };
+    }
 
 
     /*

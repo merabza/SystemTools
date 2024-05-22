5c5678366c727eab396148471e3ad6c7726e4a25
merabza <merabza@gmail.com>
2024-05-22 17:28:01 +0400

    202405221727

---
+++ TestApiContracts/TestApiClient.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using SystemToolsShared;
+using OneOf;
+
+namespace TestApiContracts;
+
+public class TestApiClient : ApiClient
+{
+    //public const string ApiName = "TestApi";
+
+    // ReSharper disable once ConvertToPrimaryConstructor
+    public TestApiClient(ILogger logger, IHttpClientFactory httpClientFactory, string server) : base(logger,
+        httpClientFactory, server, null, null, false)
+    {
+    }
+
+    public async Task<OneOf<string, Err[]>> GetAppSettingsVersion(CancellationToken cancellationToken)
+    {
+        return await GetAsyncAsString("test/getappsettingsversion", cancellationToken);
+    }
+
+    public async Task<OneOf<string, Err[]>> GetVersion(CancellationToken cancellationToken)
+    {
+        return await GetAsyncAsString("test/getversion", cancellationToken);
+    }
+}

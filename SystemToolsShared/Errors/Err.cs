d4b8d56a762ad789c9a4d97fcbfaf5e6099cd3d9
merabza <merabza@gmail.com>
2024-11-13 16:12:43 +0400

    202411131611

--- SystemToolsShared/Errors/Err.cs
+++ SystemToolsShared/Errors/Err.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace SystemToolsShared.Errors;
 
-public struct Err
+public struct Err : IEquatable<Err>
 {
     //[JsonProperty(PropertyName = "errorCode")]
     public string ErrorCode { get; set; }
@@ -35,5 +36,20 @@ public struct Err
     public static void PrintErrorsOnConsole(IEnumerable<Err> errors)
     {
         foreach (var error in errors) StShared.WriteErrorLine(error.ErrorMessage, true, null, false);
+    }
+
+    public bool Equals(Err other)
+    {
+        return ErrorCode == other.ErrorCode && ErrorMessage == other.ErrorMessage;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Err other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(ErrorCode, ErrorMessage);
     }
 }

8b1f7f8e00e79b5e12bcd881e4e6f740b239171a
merabza <merabza@gmail.com>
2025-01-17 16:58:22 +0400

    202501171656

--- SystemToolsShared/Errors/SystemToolsErrors.cs
+++ SystemToolsShared/Errors/SystemToolsErrors.cs
@@ -7,26 +7,22 @@ public static class SystemToolsErrors
 {
     public static readonly Err UnexpectedError = new()
     {
-        ErrorCode = nameof(UnexpectedError),
-        ErrorMessage = "გაუთვალისწინებელი შეცდომა"
+        ErrorCode = nameof(UnexpectedError), ErrorMessage = "გაუთვალისწინებელი შეცდომა"
     };
 
-    public static Err SuchARecordAlreadyExists => new()
-        { ErrorCode = nameof(SuchARecordAlreadyExists), ErrorMessage = "ასეთი ჩანაწერი უკვე არსებობს" };
+    public static Err SuchARecordAlreadyExists =>
+        new() { ErrorCode = nameof(SuchARecordAlreadyExists), ErrorMessage = "ასეთი ჩანაწერი უკვე არსებობს" };
 
-    public static Err TheEntryHasBeenUsedAndCannotBeDeleted => new()
-    {
-        ErrorCode = nameof(TheEntryHasBeenUsedAndCannotBeDeleted),
-        ErrorMessage = "ჩანაწერი გამოყენებულია და ვერ წაიშლება"
-    };
+    public static Err TheEntryHasBeenUsedAndCannotBeDeleted =>
+        new()
+        {
+            ErrorCode = nameof(TheEntryHasBeenUsedAndCannotBeDeleted),
+            ErrorMessage = "ჩანაწერი გამოყენებულია და ვერ წაიშლება"
+        };
 
     public static Err ErrorCaught(string methodName, string errorMessage)
     {
-        return new Err
-        {
-            ErrorCode = nameof(ErrorCaught),
-            ErrorMessage = $"Error in {methodName} {errorMessage}"
-        };
+        return new Err { ErrorCode = nameof(ErrorCaught), ErrorMessage = $"Error in {methodName} {errorMessage}" };
     }
 
     public static Err VirtualMethodOverrideNotImplemented(string methodName)
@@ -41,13 +37,17 @@ public static class SystemToolsErrors
     public static Err MethodNotImplemented(string methodName)
     {
         return new Err
-            { ErrorCode = nameof(MethodNotImplemented), ErrorMessage = $"Method {methodName} did Not Implemented" };
+        {
+            ErrorCode = nameof(MethodNotImplemented), ErrorMessage = $"Method {methodName} did Not Implemented"
+        };
     }
 
     public static Err HandlerNotImplemented(string methodName)
     {
         return new Err
-            { ErrorCode = nameof(MethodNotImplemented), ErrorMessage = $"Handler {methodName} did Not Implemented" };
+        {
+            ErrorCode = nameof(MethodNotImplemented), ErrorMessage = $"Handler {methodName} did Not Implemented"
+        };
     }
 
     public static Err ErrorWhenRunningMethod(string methodName, Guid errorGuid)
@@ -64,7 +64,9 @@ public static class SystemToolsErrors
         var errorId = Guid.NewGuid();
         Log.Error($"{errorId} - {e.Message}{Environment.NewLine}{e.StackTrace}");
         return new Err
-            { ErrorCode = nameof(UnexpectedApiException), ErrorMessage = $"გაუთვალისწინებელი შეცდომა: {errorId}" };
+        {
+            ErrorCode = nameof(UnexpectedApiException), ErrorMessage = $"გაუთვალისწინებელი შეცდომა: {errorId}"
+        };
     }
 
     public static Err RunProcessError(string errorMessage)
@@ -77,7 +79,9 @@ public static class SystemToolsErrors
         var errorId = Guid.NewGuid();
         Log.Error($"{errorId} - {e.Message}{Environment.NewLine}{e.StackTrace}");
         return new Err
-            { ErrorCode = nameof(UnexpectedDatabaseException), ErrorMessage = $"მონაცემთა ბაზასთან დაკავშირებული გაუთვალისწინებელი შეცდომა: {errorId}" };
+        {
+            ErrorCode = nameof(UnexpectedDatabaseException),
+            ErrorMessage = $"მონაცემთა ბაზასთან დაკავშირებული გაუთვალისწინებელი შეცდომა: {errorId}"
+        };
     }
-
 }

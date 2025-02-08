835a55daca3aa442ed1e1ef248c63dbfb857b0be
merabza <merabza@gmail.com>
2025-02-08 14:10:58 +0400

    202502081410

--- SystemToolsShared/ProgramAttributes.cs
+++ SystemToolsShared/ProgramAttributes.cs
@@ -1,74 +1,71 @@
-﻿//using System.Collections.Generic;
-//using System.Text;
-//using System.Threading;
+﻿using System.Threading;
 
-//namespace SystemToolsShared;
+namespace SystemToolsShared;
 
-//public sealed class ProgramAttributes
-//{
-//    private static ProgramAttributes? _instance;
-//    private static readonly Lock SyncRoot = new();
-//    private readonly Dictionary<string, object> _attributes;
+public sealed class ProgramAttributes
+{
+    private static ProgramAttributes? _instance;
 
-//    // ReSharper disable once MemberCanBePrivate.Global
-//    private ProgramAttributes()
-//    {
-//        _attributes = new Dictionary<string, object>();
-//    }
+    private static readonly Lock SyncRoot = new();
+    //private readonly Dictionary<string, object> _attributes = new();
 
-//    public static ProgramAttributes Instance
-//    {
-//        get
-//        {
-//            //ეს ატრიბუტები სესიაზე არ არის დამოკიდებული და იქმნება პროგრამის გაშვებისთანავე, 
-//            //შემდგომში მასში ცვლილებები არ შედის,
-//            //მაგრამ შეიძლება პროგრამამ თავისი მუშაობის განმავლობაში რამდენჯერმე გამოიყენოს აქ არსებული ინფორმაცია
-//            if (_instance is not null)
-//                return _instance;
-//            lock (SyncRoot) //thread safe singleton
-//            {
-//                _instance ??= new ProgramAttributes();
-//            }
+    // ReSharper disable once MemberCanBePrivate.Global
+    private ProgramAttributes()
+    {
+    }
 
-//            return _instance;
-//        }
-//    }
+    public static ProgramAttributes Instance
+    {
+        get
+        {
+            //ეს ატრიბუტები სესიაზე არ არის დამოკიდებული და იქმნება პროგრამის გაშვებისთანავე, 
+            //შემდგომში მასში ცვლილებები არ შედის,
+            //მაგრამ შეიძლება პროგრამამ თავისი მუშაობის განმავლობაში რამდენჯერმე გამოიყენოს აქ არსებული ინფორმაცია
+            if (_instance is not null)
+                return _instance;
+            lock (SyncRoot) //thread safe singleton
+            {
+                _instance ??= new ProgramAttributes();
+            }
 
-//    public string AppName { get; set; } = null!;
-//    public string AppKey { get; set; } = null!;
+            return _instance;
+        }
+    }
 
-//    public static void SetTestInstance(ProgramAttributes newInstance)
-//    {
-//        _instance = newInstance;
-//    }
+    public string AppName { get; set; } = null!;
+    public string AppKey { get; set; } = null!;
 
-//    public void SetAttribute<TC>(string attributeName, TC attributeValue) where TC : notnull
-//    {
-//        _attributes[attributeName] = attributeValue;
-//    }
+    //public static void SetTestInstance(ProgramAttributes newInstance)
+    //{
+    //    _instance = newInstance;
+    //}
 
-//    public TC? GetAttribute<TC>(string attributeName)
-//    {
-//        if (_attributes.TryGetValue(attributeName, out var attribute))
-//            return (TC)attribute;
-//        return default;
-//    }
+    //public void SetAttribute<TC>(string attributeName, TC attributeValue) where TC : notnull
+    //{
+    //    _attributes[attributeName] = attributeValue;
+    //}
 
-//    public override string ToString()
-//    {
-//        var sb = new StringBuilder();
-//        var atLeastOneAdded = false;
-//        foreach (var kvp in _attributes)
-//        {
-//            if (atLeastOneAdded)
-//                sb.Append(", ");
-//            sb.Append(kvp.Key);
-//            sb.Append('=');
-//            sb.Append(kvp.Value);
-//            atLeastOneAdded = true;
-//        }
+    //public TC? GetAttribute<TC>(string attributeName)
+    //{
+    //    if (_attributes.TryGetValue(attributeName, out var attribute))
+    //        return (TC)attribute;
+    //    return default;
+    //}
 
-//        return sb.ToString();
-//    }
-//}
+    //public override string ToString()
+    //{
+    //    var sb = new StringBuilder();
+    //    var atLeastOneAdded = false;
+    //    foreach (var kvp in _attributes)
+    //    {
+    //        if (atLeastOneAdded)
+    //            sb.Append(", ");
+    //        sb.Append(kvp.Key);
+    //        sb.Append('=');
+    //        sb.Append(kvp.Value);
+    //        atLeastOneAdded = true;
+    //    }
 
+    //    return sb.ToString();
+    //}
+}

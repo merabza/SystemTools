bc0856714179125a19138a1545ea9a8d2f5afec1
merabza <merabza@gmail.com>
2025-03-17 23:32:33 +0400

    202503172332

---
+++ JetBrainsResharperGlobalToolsWork/JetBrainsResharperGlobalToolsProcessor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using LanguageExt;
+using Microsoft.Extensions.Logging;
+using SystemToolsShared;
+using SystemToolsShared.Errors;
+
+namespace JetBrainsResharperGlobalToolsWork;
+
+public class JetBrainsResharperGlobalToolsProcessor
+{
+    private const string Jb = "jb";
+    private readonly ILogger? _logger;
+    private readonly bool _useConsole;
+
+    // ReSharper disable once ConvertToPrimaryConstructor
+    public JetBrainsResharperGlobalToolsProcessor(ILogger? logger, bool useConsole)
+    {
+        _logger = logger;
+        _useConsole = useConsole;
+    }
+
+    public Option<IEnumerable<Err>> Cleanupcode(string path, bool includeJson = false)
+    {
+        return StShared.RunProcess(_useConsole, _logger, Jb,
+            $"cleanupcode{(includeJson ? "" : " --exclude=\"**.json\"")} {path}");
+    }
+}

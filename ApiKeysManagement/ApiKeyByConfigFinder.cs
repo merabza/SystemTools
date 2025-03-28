353faf9251a186f22a3fe25c634c3c03523a90b3
merabza <merabza@gmail.com>
2025-03-28 17:44:01 +0400

    202503281744

---
+++ ApiKeysManagement/ApiKeyByConfigFinder.cs
@@ -0,0 +1,34 @@
+﻿using ApiKeysManagement.Domain;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace ApiKeysManagement;
+
+public class ApiKeyByConfigFinder : IApiKeyFinder
+{
+    private readonly ILogger<ApiKeyByConfigFinder> _logger;
+    private readonly IConfiguration _configuration;
+
+    // ReSharper disable once ConvertToPrimaryConstructor
+    public ApiKeyByConfigFinder(ILogger<ApiKeyByConfigFinder> logger, IConfiguration configuration)
+    {
+        _logger = logger;
+        _configuration = configuration;
+    }
+
+    public async Task<ApiKeyAndRemoteIpAddressDomain?> GetApiKeyAndRemAddress(string apiKey, string remoteIpAddress)
+    {
+        var apiKeys = ApiKeysDomain.Create(_configuration, _logger);
+
+        //_logger.LogInformation($"View Api Keys. count is - {apiKeys.ApiKeys.Count}");
+        //foreach (ApiKeyAndRemoteIpAddressDomain key in apiKeys.ApiKeys)
+        //{
+        //    _logger.LogInformation($"RemoteIpAddress is - {key.RemoteIpAddress}");
+        //    _logger.LogInformation($"ApiKey is - {key.ApiKey}");
+        //}
+        //_logger.LogInformation("View Api Keys Finished");
+
+        return await Task.FromResult(apiKeys.AppSettingsByApiKey(apiKey, remoteIpAddress));
+    }
+}
--- ApiKeysManagement/ApiKeysChecker.cs
+++ ApiKeysManagement/ApiKeysChecker.cs
@@ -1,65 +1,65 @@
-﻿using System.Threading.Tasks;
-using ApiContracts;
-using ApiContracts.Errors;
-using ApiKeysManagement.Domain;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+﻿//using System.Threading.Tasks;
+//using ApiContracts;
+//using ApiContracts.Errors;
+//using ApiKeysManagement.Domain;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.Logging;
 
-namespace ApiKeysManagement;
+//namespace ApiKeysManagement;
 
-// ReSharper disable once UnusedType.Global
-public sealed class ApiKeysChecker : IEndpointFilter
-{
-    private readonly IConfiguration _configuration;
-    private readonly ILogger _logger;
+//// ReSharper disable once UnusedType.Global
+//public sealed class ApiKeysChecker : IEndpointFilter
+//{
+//    private readonly IConfiguration _configuration;
+//    private readonly ILogger _logger;
 
-    // ReSharper disable once ConvertToPrimaryConstructor
-    public ApiKeysChecker(ILoggerFactory loggerFactory, IConfiguration configuration)
-    {
-        _configuration = configuration;
-        _logger = loggerFactory.CreateLogger<ApiKeysChecker>();
-    }
+//    // ReSharper disable once ConvertToPrimaryConstructor
+//    public ApiKeysChecker(ILoggerFactory loggerFactory, IConfiguration configuration)
+//    {
+//        _configuration = configuration;
+//        _logger = loggerFactory.CreateLogger<ApiKeysChecker>();
+//    }
 
-    public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
-    {
-        var apiKey = context.HttpContext.Request.Query[ApiKeysConstants.ApiKeyParameterName].ToString();
-        var remoteAddress = context.HttpContext.Connection.RemoteIpAddress;
+//    public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+//    {
+//        var apiKey = context.HttpContext.Request.Query[ApiKeysConstants.ApiKeyParameterName].ToString();
+//        var remoteAddress = context.HttpContext.Connection.RemoteIpAddress;
 
-        if (remoteAddress is null)
-            return Results.BadRequest(new[] { ApiErrors.InvalidRemoteAddress });
+//        if (remoteAddress is null)
+//            return Results.BadRequest(new[] { ApiErrors.InvalidRemoteAddress });
 
-        if (!Check(apiKey, remoteAddress.MapToIPv4().ToString()))
-            return Results.BadRequest(new[] { ApiErrors.ApiKeyIsInvalid });
+//        if (!Check(apiKey, remoteAddress.MapToIPv4().ToString()))
+//            return Results.BadRequest(new[] { ApiErrors.ApiKeyIsInvalid });
 
-        return await next(context);
-    }
+//        return await next(context);
+//    }
 
-    private bool Check(string? apiKey, string remoteIpAddress)
-    {
-        if (string.IsNullOrWhiteSpace(apiKey))
-            return false;
+//    private bool Check(string? apiKey, string remoteIpAddress)
+//    {
+//        if (string.IsNullOrWhiteSpace(apiKey))
+//            return false;
 
-        var apiKeys = ApiKeysDomain.Create(_configuration, _logger);
+//        var apiKeys = ApiKeysDomain.Create(_configuration, _logger);
 
-        //var apiKeysCount = apiKeys.ApiKeys.Count;
-        //_logger.LogInformation("View Api Keys. count is - {apiKeysCount}", apiKeysCount);
-        //foreach (ApiKeyAndRemoteIpAddressDomain key in apiKeys.ApiKeys)
-        //{
-        //    var keyRemoteIpAddress = key.RemoteIpAddress;
-        //    _logger.LogInformation("RemoteIpAddress is - {keyRemoteIpAddress}", keyRemoteIpAddress);
-        //    var keyApiKey = key.ApiKey;
-        //    _logger.LogInformation("ApiKey is - {keyApiKey}", keyApiKey);
-        //}
+//        //var apiKeysCount = apiKeys.ApiKeys.Count;
+//        //_logger.LogInformation("View Api Keys. count is - {apiKeysCount}", apiKeysCount);
+//        //foreach (ApiKeyAndRemoteIpAddressDomain key in apiKeys.ApiKeys)
+//        //{
+//        //    var keyRemoteIpAddress = key.RemoteIpAddress;
+//        //    _logger.LogInformation("RemoteIpAddress is - {keyRemoteIpAddress}", keyRemoteIpAddress);
+//        //    var keyApiKey = key.ApiKey;
+//        //    _logger.LogInformation("ApiKey is - {keyApiKey}", keyApiKey);
+//        //}
 
-        //_logger.LogInformation("View Api Keys Finished");
+//        //_logger.LogInformation("View Api Keys Finished");
 
-        var ak = apiKeys.AppSettingsByApiKey(apiKey, remoteIpAddress);
+//        var ak = apiKeys.AppSettingsByApiKey(apiKey, remoteIpAddress);
 
-        if (ak != null)
-            return true;
-        _logger.LogError("RemoteIpAddress is - {remoteIpAddress}", remoteIpAddress);
-        _logger.LogError("API Key is invalid - {apiKey}", apiKey);
-        return false;
-    }
-}
+//        if (ak != null)
+//            return true;
+//        _logger.LogError("RemoteIpAddress is - {remoteIpAddress}", remoteIpAddress);
+//        _logger.LogError("API Key is invalid - {apiKey}", apiKey);
+//        return false;
+//    }
+//}

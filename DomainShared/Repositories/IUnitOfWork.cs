0b78ffb8f93e15655afe0037754265d9c5adad1f
merabza <merabza@gmail.com>
2026-01-10 19:28:04 +0400

    202601101925

--- DomainShared/Repositories/IUnitOfWork.cs
+++ DomainShared/Repositories/IUnitOfWork.cs
@@ -1,6 +1,8 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
+using LanguageExt;
 using Microsoft.EntityFrameworkCore.Storage;
+using SystemToolsShared.Errors;
 
 namespace DomainShared.Repositories;
 
@@ -9,4 +11,6 @@ public interface IUnitOfWork
     Task SaveChangesAsync(CancellationToken cancellationToken = default);
     string GetTableName<T>() where T : class;
     Task<IDbContextTransaction> BeginTransactionAsync(CancellationToken cancellationToken = default);
+    Task<Option<Err[]>> ExecuteSqlRawRetOptionAsync(string sql, CancellationToken cancellationToken = default);
+
 }

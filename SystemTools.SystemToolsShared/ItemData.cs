da1103c77e51351072522aeffa3f2e61e890cad9
merabza <merabza@gmail.com>
2026-02-14 15:14:26 +0400

    202602141510

---
+++ SystemTools.SystemToolsShared/ItemData.cs
@@ -0,0 +1,10 @@
+﻿namespace SystemTools.SystemToolsShared;
+
+//masterData-ს ელემენტის ბაზური კლასი
+public /*open*/ class ItemData
+{
+    public virtual string? GetItemKey()
+    {
+        return null;
+    }
+}

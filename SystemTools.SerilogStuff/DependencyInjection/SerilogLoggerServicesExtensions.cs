013527c53570de9be0148c2c62d4dd2b9d2b9d79
merabza <merabza@gmail.com>
2026-05-14 14:54:30 +0400

    202605141453

---
+++ SystemTools.SerilogStuff/DependencyInjection/SerilogLoggerServicesExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+
+namespace SystemTools.SerilogStuff.DependencyInjection;
+
+public static class SerilogLoggerServicesExtensions
+{
+    public static IServiceCollection AddSerilogLoggerService(this IServiceCollection services,
+        LogEventLevel consoleLogEventLevel, string appName, string? logFolder = null, string? logFileName = null)
+    {
+        SerilogLoggerCreator.CreateLogger(consoleLogEventLevel, appName, logFileName, logFolder);
+        services.AddLogging(configure => configure.AddSerilog());
+        services.Configure<LoggerFilterOptions>(options => options.MinLevel = LogLevel.Information);
+        return services;
+    }
+
+}
---
+++ SystemTools.SerilogStuff/SerilogLoggerCreator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using Serilog;
+using Serilog.Events;
+
+namespace SystemTools.SerilogStuff;
+
+public static class SerilogLoggerCreator
+{
+    public static void CreateLogger(LogEventLevel consoleLogEventLevel, string appName, string? logFileName,
+        string? logFolder)
+    {
+        try
+        {
+            string? logFile = null;
+            if (!string.IsNullOrWhiteSpace(logFileName))
+            {
+                logFile = logFileName;
+            }
+            else if (logFolder is not null)
+            {
+                logFile = Path.Combine(logFolder, appName, $"{appName}.log");
+            }
+
+            if (logFile is null)
+            {
+                return;
+            }
+
+            const string extension = ".log";
+            if (logFile.ToUpperInvariant().EndsWith(".LOG", StringComparison.Ordinal) ||
+                logFile.ToUpperInvariant().EndsWith(".TXT", StringComparison.Ordinal))
+            {
+                logFile = logFile[..^4];
+            }
+
+            logFile += extension;
+            Log.Logger = new LoggerConfiguration().WriteTo
+                .Console(consoleLogEventLevel, formatProvider: CultureInfo.InvariantCulture).WriteTo.File(logFile,
+                    encoding: Encoding.UTF8, rollingInterval: RollingInterval.Day,
+                    formatProvider: CultureInfo.InvariantCulture).CreateLogger();
+        }
+        catch (Exception)
+        {
+            // ignored
+        }
+    }
+
+}

d0e0a95da241a08fca037a867784773e139fe88f
merabza <merabza@gmail.com>
2026-05-19 16:10:29 +0400

    202605191608

---
+++ SystemTools.SystemToolsShared/DependencyInjection/StrategyCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AppCliTools.CliMenu.DependencyInjection;
+
+public static class StrategyCollectionExtensions
+{
+    public static IServiceCollection AddTransientAllStrategies<TStrategy>(this IServiceCollection services,
+        params Assembly[] assemblies)
+    {
+        foreach (Assembly assembly in assemblies)
+        {
+            foreach (Type type in assembly.ExportedTypes.Where(x =>
+                         typeof(TStrategy).IsAssignableFrom(x) && x is { IsInterface: false, IsAbstract: false }))
+            {
+                services.AddTransient(typeof(TStrategy), type);
+            }
+        }
+
+        return services;
+    }
+}

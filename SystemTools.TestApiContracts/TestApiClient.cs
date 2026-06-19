088c0edc5e25f31a0d2b93415e5e35abc9acaf1b
merabza <merabza@gmail.com>
2026-06-19 07:17:33 +0400

    202606190713

--- SystemTools.TestApiContracts/TestApiClient.cs
+++ SystemTools.TestApiContracts/TestApiClient.cs
@@ -11,7 +11,6 @@ namespace SystemTools.TestApiContracts;
 
 public sealed class TestApiClient : ApiClient
 {
-
     // ReSharper disable once ConvertToPrimaryConstructor
     public TestApiClient(ILogger logger, IHttpClientFactory httpClientFactory, string server, bool useConsole) : base(
         logger, httpClientFactory, server, null, null, useConsole)

2712bc1eb2373c885e741785f938070d7d52fe79
merabza <merabza@gmail.com>
2026-09-08 12:18:04 +0400

    202609081218

--- SystemTools.MediatRMessagingAbstractions/ICommandHandlerOmd.cs
+++ SystemTools.MediatRMessagingAbstractions/ICommandHandlerOmd.cs
@@ -3,8 +3,7 @@ using SystemTools.SharedKernel;
 
 namespace SystemTools.MediatRMessagingAbstractions;
 
-public interface ICommandHandlerOmd<in TCommand> : IRequestHandler<TCommand, Result>
-    where TCommand : ICommandOmd;
+public interface ICommandHandlerOmd<in TCommand> : IRequestHandler<TCommand, Result> where TCommand : ICommandOmd;
 
 public interface ICommandHandlerOmd<in TCommand, TResponse> : IRequestHandler<TCommand, Result<TResponse>>
     where TCommand : ICommandOmd<TResponse>;
